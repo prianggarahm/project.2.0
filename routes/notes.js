5ec9b29dcca0a10d35d29b7927c5c21744fad75e
prianggarahm <priangga.rahmawanto@gmail.com>
2023-02-10 05:22:43 +0700

    validator and POST

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const Validator = require("fastest-validator");
+const v = new Validator();
+const { Notes } = require("../models");
 
 /* GET notes page. */
 router.get("/", function (req, res, next) {
@@ -9,6 +12,26 @@ router.get("/", function (req, res, next) {
 // COPY
 router.get("/env", function (req, res, next) {
   res.send(process.env.APP_NAME);
+});
+
+// POST
+router.post("/", async (req, res, next) => {
+  // validasi
+  const schema = {
+    title: "string",
+    description: "string|optional",
+  };
+  const validate = v.validate(req.body, schema);
+  if (validate.length) {
+    return res.status(400).json(validate);
+  }
+  // proses create
+  const note = await Notes.create(req.body);
+  res.json({
+    status: 200,
+    message: "Success create data",
+    data: note,
+  });
 });
 
 module.exports = router;
